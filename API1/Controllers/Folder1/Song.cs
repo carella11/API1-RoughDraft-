92b39f894849403b524bf315a186a0d5dd11785b
carella11 <christianar@getacademy.no>
2022-06-28 10:26:30 +0200

    Add project files.

---
+++ API1/Controllers/Folder1/Song.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace API1.Controllers.Folder1
+{
+    public class Song
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }
+        public string Artist { get; set; }
+    }
+}
---
+++ API1/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API1.Controllers.Folder1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<Song> GetOne(int id)
+        {
+            return await Task.Run(()=>
+            new Song() { Id = id, Artist = "Flume", Name = "Ezra"}
+            );
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Song>> GetMany()
+        {
+            return await Task.Run(() => new[]
+            {
+                new Song() { Id = 1, Artist = "J Cole", Name = "Lights Please" },
+                new Song() { Id = 2, Artist = "Kojey Radical", Name = "Water" }
+            }
+                    );
+        }
+        [HttpPost]
+        public async Task<int> Create(Song song)
+        {
+            return await Task.Run(() => 1);
+        }
+        [HttpDelete]
+        public async Task<int> Delete(int id)
+        {
+            return await Task.Run(() => 1);
+        }
+    }
+}
